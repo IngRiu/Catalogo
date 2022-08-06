8967bc6ef07f541841993edf2c137408d3e26cf1
joseurielaybarhernandez <joseurielaybarhernandez@gmail.com>
2022-08-06 14:26:23 -0500

    Mostrando las imagens cargadas de cada producto con laravel y bootstrap

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,5 +1,19 @@
 <div class="card">
-	<img class="card-img-top" src="{{ asset($product->images->first()->path) }}" height="500">
+	<div id="carousel{{ $product->id }}" class="carousel slide carousel-fade">
+		<div class="carousel-inner">
+			@foreach($product->images as $image)
+				<div class="carousel-item {{ $loop->first ? 'active' : '' }}">
+					<img class="d-block w-100 card-img-top" src="{{ asset($image->path) }}" height="500">
+				</div>
+			@endforeach
+		</div>
+		<a class="carousel-control-prev" href="#carousel{{ $product->id }}" role="button" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon"></span>
+		</a>
+		<a class="carousel-control-next" href="#carousel{{ $product->id }}" role="button" data-bs-slide="next">
+			<span class="carousel-control-next-icon"></span>
+		</a>
+	</div>
 	<div class="card-body">
 		<h4 class="text-right"><strong> $ {{ $product->Price }}</strong></h4>
 		<h5 class="card-title">{{ $product->Title }}</h5>
